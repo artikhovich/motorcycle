28ecf63ff9a85de309bc78c5ea041be594bb9bf9
artsel <artikhovich.by@gmail.com>
2021-01-21 17:11:26 +0300

    player moving add

--- js/app.js
+++ js/app.js
@@ -15,10 +15,37 @@ var noise = x => {
 	x = x*0.01 % 255;
 	return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
 }
+
+var player = new function () {
+	this.x = c.width/2;
+	this.y = 0;
+	this.ySpeed = 0;
+	this.rot = 0;
+	this.img = new Image();
+	this.img.src = "moto.png";
+
+	this.draw = function(){
+		var p1 = c.height - noise(t + this.x) * 0.25;
+		if (p1-15 > this.y){
+			this.ySpeed += 0.1;
+		} else {
+			this.y = p1-15;
+			this.ySpeed -= this.y - (p1-12);
+		}
+
+		this.y += this.ySpeed;
+
+		ctx.save();
+		ctx.translate(this.x, this.y);
+		ctx.drawImage(this.img, -15, -15,30,30);
+		ctx.restore();
+	}
+}
+
 var t = 0;
 
 function loop() {
-	t += 1;
+	t += 5;
 	ctx.fillStyle = "#19f";
 	ctx.fillRect(0,0,c.width, c.height);
 
@@ -32,6 +59,7 @@ function loop() {
 	ctx.lineTo(c.width, c.height);
 	ctx.fill();
 
+	player.draw();
 	requestAnimationFrame(loop);
 }
 
