cb76b1e62c273a254c7d5855ac69d9766d27fae1
artsel <artikhovich.by@gmail.com>
2021-01-21 17:30:34 +0300

    BIKE Rotated...

--- js/app.js
+++ js/app.js
@@ -20,25 +20,36 @@ var player = new function () {
 	this.x = c.width/2;
 	this.y = 0;
 	this.ySpeed = 0;
+	this.rSpeed = 0;
 	this.rot = 0;
+
 	this.img = new Image();
 	this.img.src = "moto.png";
 
 	this.draw = function(){
 		var p1 = c.height - noise(t + this.x) * 0.25;
 		var p2 = c.height - noise(t+5 + this.x) *0.25;
+		var grounded = 0;
 
 		if (p1-15 > this.y){
 			this.ySpeed += 0.1;
 		} else {
 			this.ySpeed -= this.y - (p1-15);
 			this.y = p1 - 15;
+			grounded = 1;
 		}
+		var angle = Math.atan2((p2-15) - this.y, (this.x+5) - this.x);
 
 		this.y += this.ySpeed;
+		if (grounded) {
+			this.rot -= (this.rot - angle) * 0.5;
+			this.rSpeed = this.rSpeed - (angle - this.rot);
+		}
+		this.rot -= this.rSpeed;
 
 		ctx.save();
 		ctx.translate(this.x, this.y);
+		ctx.rotate(this.rot);
 		ctx.drawImage(this.img, -15, -15, 30, 30);
 		ctx.restore();
 	}
